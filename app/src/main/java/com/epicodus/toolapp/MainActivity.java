348b03bc66834d70a31f0d1de624a8e42af0ceb3
jeremy <jeremy.rmrr@gmail.com>
2015-11-03 15:55:45 -0800

    Add on click listener to next band member button, image and content to resulting page

--- app/src/main/java/com/epicodus/toolapp/MainActivity.java
+++ app/src/main/java/com/epicodus/toolapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.toolapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +9,36 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBandMembersButton;
+    private Button mDiscographyButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBandMembersButton = (Button) findViewById(R.id.bandMemberButton);
+        mDiscographyButton = (Button) findViewById((R.id.discographyButton));
+
+        mBandMembersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BandMemberActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mDiscographyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, DiscographyActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
